dc66f813b0ed38a8384ab3cd8801e0be4a5fd0f3
Kodam Sai Charan <132771610+CoderLibrary@users.noreply.github.com>
2024-02-25 16:27:56 +0530

    Add files via upload
    First Commit

---
+++ Anagram.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+
+        String s1 = "A Gentle man";
+        String s2 = "Elegant Man";
+        s1 = s1.replace(" ", "").toLowerCase();
+        s2 = s2.replace(" ", "").toLowerCase();
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        if (Arrays.equals(c1, c2)) {
+            System.out.println("It is anagram");
+        } else {
+            System.out.println("It is not anagram");
+        }
+    }
+}
